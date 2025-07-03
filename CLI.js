8d949d99d7e2e018aac55590f0531109ab6193e8
snehagoyal25 <goyalsneha995@gmail.com>
2025-07-03 15:47:42 +0530

    commander

--- CLI.js
+++ CLI.js
@@ -28,35 +28,54 @@
 // const path = require('path');
 // const { Command } = require('commander');
 
+
+// Importing modules 
+// fs 👉 to read the file
+// path 👉 to convert a relative file path to an absolute one
+// commander 👉 to make CLI commands easy to write and use
 import fs from 'fs';
 import path from 'path';
 import { Command } from 'commander';
-
-
+//Create CLI App
 const program = new Command();
-
+//App Info 
+// You're telling Commander:
+// The name of your CLI tool is word-counter
+// What it does (help message)
+// The version of your app
 program
   .name('word-counter')
   .description('CLI to count words in a file')
   .version('1.0.0');
-
+//Define a count Command
 program
   .command('count')
   .description('Count words in the given file')
   .argument('<filePath>', 'Path to your file')
+  //What Happens When count Runs?
+//Takes the user-given path (a.txt)
+//Converts it into a full system path so Node can find it anywhere
+//Example:
+//If you're in Desktop/100, a.txt becomes C:/Users/Dell/Desktop/100/a.txt
+
   .action((filePath) => {
     const absolutePath = path.resolve(filePath);
+    //Read the File
     fs.readFile(absolutePath, 'utf8', (err, data) => {
       if (err) {
         console.error('❌ Error reading file:', err.message);
         return;
       }
-
+//Count the Words
+// data.trim() – remove extra white spaces from start/end
+// .split(/\s+/) – splits text on any kind of whitespace (space, tabs, newlines)
+// .length – gives the total number of "words"
       const wordCount = data.trim().split(/\s+/).length;
       console.log(`✅ You have ${wordCount} words in this file.`);
     });
   });
 
+  //This tells commander: "Now take input from the terminal and run commands."
 program.parse();
 
 // use this command to run : node CLI.js count a.txt
