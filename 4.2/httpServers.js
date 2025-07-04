14701913fae3caea02a1a75b3cd963b46450e16f
snehagoyal25 <goyalsneha995@gmail.com>
2025-07-04 14:54:55 +0530

    server

---
+++ 4.2/httpServers.js
@@ -0,0 +1,10 @@
+//https://petal-estimate-4e9.notion.site/Intro-to-HTTP-26c5803f153b4401aa76e9fac08ac427s //s
+// You need to deploy the websites on port 443.-> if not , it will be included in an ugly pattern.
+
+// CRUD operation -> post , get , put , delete are known as methods.  
+// Create = Post request 
+// Read = Get request 
+// Update = Put request
+// Delete = Delete request 
+// If we are using https , it means it is pointing to port 443 by default 
+// HTTPS : 443 , // HTTP : 80 . // SSH : 22 
