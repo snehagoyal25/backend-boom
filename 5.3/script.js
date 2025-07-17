a4864fb10865ad32e81bc2064cc565e30739bdc9
snehagoyal25 <goyalsneha995@gmail.com>
2025-07-17 23:42:06 +0530

    cors

--- 5.3/script.js
+++ 5.3/script.js
@@ -1,3 +1,7 @@
+// assignment 
+// create a backend server in nodejs , that returns the sum endpoint 
+// Write an HTML file, that hits the backend server using the 'fetch' API .
+
 // npm init -y
 // npm install express
 // npm install cors 
@@ -7,8 +11,11 @@ const cors = require("cors");
 
 const app = express();
 const PORT = 3004;
-
 app.use(cors());
+// app.use(cors({
+//     // domains :["www.google.com","https://aaru-frontend.vercel.app"] // This says only allow this domain
+// can also add multiple domains . 
+// }));
 app.use(express.json());
 
 app.post("/sum", (req, res) => {
