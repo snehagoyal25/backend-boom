58a9a5996f48c8d5c4e97e43ac8d487c128f8950
snehagoyal25 <goyalsneha995@gmail.com>
2025-07-20 00:02:56 +0530

    authentication

--- 6.1/httpDeepDive.js
+++ 6.1/httpDeepDive.js
@@ -2,6 +2,18 @@
 
 const express = require("express");
 const app = express();
+app.use(express.json()); // When a client (like Postman, browser, or frontend app) sends JSON data in a request body (usually with POST or PUT), this line ensures your Express app can read and access that data using req.body.
+// 📦 Why it's needed?
+// Because Node.js doesn’t parse incoming JSON by default.
+// This middleware:
+// Reads raw incoming request
+// Parses the JSON
+// Adds it to req.body for easy use
+
+
+//In older versions of Express (<4.16), you had to install body-parser separately. Now, express.json() is built-in.
+
+
 // Create two new POST routes, one for signing up and one for signing in. 
 function signinHandler(req,res){
 
@@ -11,7 +23,8 @@ function signupHandler(req,res){
 
 }
 
-app.post("/signup",signinHandler)
+
 app.post("/signin",signupHandler)
+app.post("/signup",signinHandler)
 
 app.listen(3000);// that the http server is running on port 3000
