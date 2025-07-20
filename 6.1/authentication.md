3749e8cd4356df77cf7b92d46ff3d87fe526233a
snehagoyal25 <goyalsneha995@gmail.com>
2025-07-20 19:21:04 +0530

    authentication

---
+++ 6.1/authentication.md
@@ -0,0 +1,13 @@
+app.use(express.json()); 
+
+// When a client (like Postman, browser, or frontend app) sends JSON data in a request body (usually with POST or PUT), this line ensures your Express app can read and access that data using req.body.
+
+// 📦 Why it's needed?
+
+// Because Node.js doesn’t parse incoming JSON by default.
+// This middleware:
+// Reads raw incoming request
+// Parses the JSON
+// Adds it to req.body for easy use
+
+//In older versions of Express (<4.16), you had to install body-parser separately. Now, express.json() is built-in.
--- 6.1/httpDeepDive.js
+++ 6.1/httpDeepDive.js
@@ -2,29 +2,113 @@
 
 const express = require("express");
 const app = express();
-app.use(express.json()); // When a client (like Postman, browser, or frontend app) sends JSON data in a request body (usually with POST or PUT), this line ensures your Express app can read and access that data using req.body.
-// 📦 Why it's needed?
-// Because Node.js doesn’t parse incoming JSON by default.
-// This middleware:
-// Reads raw incoming request
-// Parses the JSON
-// Adds it to req.body for easy use
+app.use(express.json()); 
 
+const users=[];
 
-//In older versions of Express (<4.16), you had to install body-parser separately. Now, express.json() is built-in.
+   function generateToken() {
+    let options = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+    ];
+
+    let token = "";
+    for (let i = 0; i < 32; i++) {
+        token += options[Math.floor(Math.random() * options.length)];
+    }
+
+    return token;
+}
+
+// Example usage:
+console.log(generateToken());
 
 
 // Create two new POST routes, one for signing up and one for signing in. 
-function signinHandler(req,res){
-
-}
-
 function signupHandler(req,res){
+ const username = req.body.username;
+ const password = req.body.password;
 
+ if(users.find(u=>u.username===username)){
+    res.json({
+        message :"You have already signed up"
+    })
+ }
+ users.push({
+    username : username,
+    password : password
+ })
+
+ res.json({
+    message : "You have signed in "
+ })
+ console.log(users)
+}
+
+function signinHandler(req,res){
+ const username = req.body.username;
+ const password = req.body.password;
+
+ let foundUser = null;
+ for(let i=0;i<users.length;i++){
+    if(users[i].username===username && users[i].password===password){
+       foundUser = users[i];
+    }
+ }
+
+ if(foundUser){
+    const token = generateToken();
+    foundUser.token = token // This line of code not generate username and password only that is generating prior but now this line of code will gwnwrate a 32 character token as a response to the user .
+    res.json({
+        message : token
+    })
+    console.log(users);
+ }
+ else{
+res.status(403).send({
+    message :"Invalid username or password"
+})
+console.log(users)
+ }
+// or
+//  const user = user.find(function(u){
+//     if(u.username==username && u.password==password){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+//  })
 }
 
 
-app.post("/signin",signupHandler)
-app.post("/signup",signinHandler)
+app.post("/signup",signupHandler)
+app.post("/signin",signinHandler)
 
-app.listen(3000);// that the http server is running on port 3000
+// Lets create an endpoint (/me) that returns the user their information 'only if they send their . 
+app.get("/me",function(req,res){
+    const token = req.headers.token
+    let foundUser = null;
+
+    for(let i=0;i<users.length;i++){
+        if(users[i].token==token){
+            foundUser=users[i]
+        }    
+    }
+    if(foundUser){
+res.json({
+    username : foundUser.username,
+    password : foundUser.password
+})
+    }
+    else{
+            res.status(401).send({
+                message :"unauthorized user "
+            })
+        }
+
+})
+app.listen(3001);// that the http server is running on port 3000
