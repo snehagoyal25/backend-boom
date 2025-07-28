58bdfb3ebea8a3989fc7ce2a9a286d592ff03f69
snehagoyal25 <goyalsneha995@gmail.com>
2025-07-28 20:27:41 +0530

    authentication

---
+++ 6.2/assignment.js
@@ -0,0 +1,3 @@
+// Assignment : Create a auth middleware
+// https://petal-estimate-4e9.notion.site/Authentincation-a4b43c7cc1d14535a7b5b366080095fas //s 
+// it says :- Can you try creating a middleware called auth that verifies if a user is logged in and ends the request early if the user isn't login .
