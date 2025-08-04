a95487aa37cc2a5edddeab0e6108157cb8e5868e
snehagoyal25 <goyalsneha995@gmail.com>
2025-08-04 23:21:21 +0530

    mongo

--- 7.1/mongo.js
+++ 7.1/mongo.js
@@ -1,1 +1,9 @@
 // https://petal-estimate-4e9.notion.site/Databases-and-MongoDb-1017dfd107358065a996cda5ed89682es //s 
+
+// backend servers are transient", it means that they are not permanent — they can be stopped, restarted, replaced, or destroyed at any time.
+//If a server crashes, a new one may replace it automatically.
+
+// "Servers handle tasks and give responses(they should be stateless), but only databases keep the information stored safely even if the server stops or changes." ✅
+
+
+
